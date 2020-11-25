a3e430130040f01a47bbb4c893f77679cd256289
dasha-ryzhkova <ryzhkovadasha2002@gmail.com>
2020-11-25 23:58:51 +0200

    fixed

--- venv/Task.py
+++ venv/Task.py
@@ -5,9 +5,9 @@ print("Input path to file:")
 f = input()
 print("Input path to save image:")
 p = input()
+# f = "C:\\Users\\VivoBook\\Desktop\\KGMlab2\\DS5.txt"
+# p = "C:\\Users\\VivoBook\\Desktop\\KGMlab2\\test.png"
 
-#C:\Users\\VivoBook\Desktop\\KGMlab2\DS5.txt
-#C:\Users\\VivoBook\Desktop\\KGMlab2\test.png
 
 file = open(f, "r")
 line = file.readlines()
@@ -17,8 +17,8 @@ for i in line:
     dot.append(int(cord_x))
     dot.append(int(cord_y))
 file.close()
-
-image = Image.new("RGBA", (540, 960), (0,0,0,0))
+image = Image.new("RGBA", (960, 960), (0,0,0,0))
 draw = ImageDraw.Draw(image)
 draw.point(dot, fill = "black")
 image.save(p, "PNG")
+image.show()
